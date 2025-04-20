57bc6452054353e9aac4b7c06a6eeea2753bbd12
David Lopera <dein4267@gmail.com>
2025-04-19 21:12:26 -0500

    first commit

---
+++ Assets/Scripts/LiquidCharacterController.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidCharacterController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    
+    public enum CharacterShape { Sphere, Cube, Flat }
+
+    [Header("Shape Changing")]  // <- Colocado aquí, justo antes de la variable
+    public CharacterShape currentShape = CharacterShape.Sphere;
+    
+    // References to different shape meshes/objects
+    public GameObject sphereShape;
+    public GameObject cubeShape;
+    public GameObject flatShape;
+    
+    private Rigidbody rb;
+    private bool isGrounded = true;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        UpdateCharacterShape();
+    }
+    
+    void Update()
+    {
+        // Handle movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(horizontalInput, 0, 0) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+        
+        // Handle jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+        
+        // Handle shape changing
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeShape(CharacterShape.Sphere);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeShape(CharacterShape.Cube);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChangeShape(CharacterShape.Flat);
+        }
+    }
+    
+    void ChangeShape(CharacterShape newShape)
+    {
+        currentShape = newShape;
+        UpdateCharacterShape();
+    }
+    
+    void UpdateCharacterShape()
+    {
+        // Disable all shapes first
+        sphereShape.SetActive(false);
+        cubeShape.SetActive(false);
+        flatShape.SetActive(false);
+        
+        // Enable the current shape
+        switch (currentShape)
+        {
+            case CharacterShape.Sphere:
+                sphereShape.SetActive(true);
+                break;
+            case CharacterShape.Cube:
+                cubeShape.SetActive(true);
+                break;
+            case CharacterShape.Flat:
+                flatShape.SetActive(true);
+                break;
+        }
+        
+        // Update the collider to match the new shape
+        UpdateCollider();
+    }
+    
+    void UpdateCollider()
+    {
+        // Remove existing colliders
+        Collider[] colliders = GetComponents<Collider>();
+        foreach (Collider c in colliders)
+        {
+            Destroy(c);
+        }
+        
+        // Add the appropriate collider based on the shape
+        switch (currentShape)
+        {
+            case CharacterShape.Sphere:
+                SphereCollider sphereCol = gameObject.AddComponent<SphereCollider>();
+                sphereCol.radius = 1.25f;
+                sphereCol.center = new Vector3(0, -0.1f, 0); // Ajusta el centro ligeramente hacia abajo
+                break;
+                
+            case CharacterShape.Cube:
+                BoxCollider boxCol = gameObject.AddComponent<BoxCollider>();
+                boxCol.center = new Vector3(0, 1, -1); // Centro coincide con la posición
+                boxCol.size = new Vector3(0.7f, 4f, 1f); // Mismo tamaño que la escala
+                break;
+                
+            case CharacterShape.Flat:
+                BoxCollider flatCol = gameObject.AddComponent<BoxCollider>();
+                flatCol.center = new Vector3(0, 0, -0.8f); // Centro coincide con la posición
+                flatCol.size = new Vector3(6f, 2f, 1f); // Mismo tamaño que la escala
+                break;
+        }
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if we've landed on something
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
+    }
+}
